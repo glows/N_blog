62b407fc2961d3e5d9f6d8bf96f2b0c7cfa58fb4
duncyun <lawmusil@163.com>
2017-12-29 18:17:55 +0800

    添加数据库

--- config/default.js
+++ config/default.js
@@ -5,5 +5,6 @@ module.exports = {
       key: '内容分发系统',
       maxAge: 2592000000
     },
-    mongodb: 'mongodb://localhost:27017/myblog'
+    // mongodb: 'mongodb://localhost:27017/myblog',
+    mongodb: 'mongodb://s7even:y7520@ds133017.mlab.com:33017/y7'
   };
